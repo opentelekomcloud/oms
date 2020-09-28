55cd748e55034605fac49666378976ce782e3213
Anton Kachurin <anton.kachurin@t-systems.com>
2020-09-29 00:21:14 +0300

    Add client integration tests (#9)
    Improve mock server
    
    Add test npm script: clean + unit + integration + functional + report
    
    Fix minor problems

--- tests/integration/client.test.ts
+++ tests/integration/client.test.ts
@@ -1,6 +1,9 @@
-import { CloudConfigHelper } from '../../src/oms/core/types';
-import { authServerUrl, fakeAuthServer, fakeRegion, fakeServiceServer } from '../utils/servers'
-import Client from '../../src/oms/client';
+import { CloudConfig, CloudConfigHelper } from '../../src/oms/core/types'
+import { authServerUrl, fakeAuthServer, fakeRegion, fakeServiceServer, fakeToken } from '../utils/servers'
+import Client from '../../src/oms/client'
+import { randomString } from '../utils/helpers'
+import Service from '../../src/oms/services/base'
+import HttpClient from '../../src/oms/core/http'
 
 beforeAll(() => {
     fakeAuthServer.listen()
@@ -12,10 +15,85 @@ afterAll(() => {
     fakeServiceServer.close()
 })
 
-test('Client_auth', async () => {
+test('Client: authToken', async () => {
+    const cfg = new CloudConfigHelper(authServerUrl())
+        .simplePasswordConfig('MYDOMAIN', 'MYNAME', '>>>Super!Secret<<<', fakeRegion)
+    const client = new Client(cfg)
+    await client.authToken()
+})
+
+test('Client: authAKSK', () => {
+    const cfg = new CloudConfigHelper(authServerUrl())
+        .simpleAkSkConfig('AK', 'SK')
+    const client = new Client(cfg)
+    client.authAkSk()
+})
+
+const srv = {
+    type: randomString(5),
+    version: 'v6',
+    url: 'https://' + randomString(12),
+}
+
+test('Client: register service', () => {
+    const cfg = new CloudConfigHelper(authServerUrl())
+        .simpleTokenConfig(fakeToken)
+    const client = new Client(cfg)
+    client.registerService(srv.type, srv.version, srv.url)
+
+    class pseudo extends Service {
+        static version = srv.version
+        static type = srv.type
+
+        constructor(url: string, client: HttpClient) {
+            super(url, client)
+        }
+    }
+
+    const ps = client.getService(pseudo)
+    expect(ps).toBeDefined()
+    expect(ps.client.baseConfig.baseURL).toBe(srv.url)
+})
+
+test('Client: get not registered service', async () => {
     const cfg = new CloudConfigHelper(authServerUrl())
         .simplePasswordConfig('MYDOMAIN', 'MYNAME', '>>>Super!Secret<<<', fakeRegion)
     const client = new Client(cfg)
     await client.authenticate()
-    await client.loadServiceCatalog()
+
+    class pseudo extends Service {
+        static version = srv.version
+        static type = srv.type
+
+        constructor(url: string, client: HttpClient) {
+            super(url, client)
+        }
+    }
+
+    expect(() => client.getService(pseudo)).toThrowError()
+})
+
+test('Client: no auth opts', async () => {
+    const cfg = new CloudConfig()
+    const client = new Client(cfg)
+    await expect(client.authenticate()).rejects.toThrowError()
+})
+
+test('Client: no ak/sk opts', () => {
+    const cfg = { auth: { auth_url: 'http://notempty' } }
+    const client = new Client(cfg)
+    expect(() => client.authAkSk()).toThrowError()
+})
+
+test('Client: ak/sk opts', async () => {
+    const cfg = new CloudConfigHelper('http://nsdfdf').simpleAkSkConfig(
+        randomString(10),
+        randomString(20),
+    )
+    const client = new Client(cfg)
+    client.authAkSk = jest.fn()
+    await client.authenticate().catch(() => {
+        console.log('This is fine')
+    })
+    expect(client.authAkSk).toBeCalled()
 })
