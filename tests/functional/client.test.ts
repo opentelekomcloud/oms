ecdd5da33bbf6c915598a6479a9416fb6abfb5ca
Anton Kachurin <anton.kachurin@t-systems.com>
2020-10-09 10:22:02 +0300

    Remove available service restrictions (#35)
    Remove available service restrictions
    
    Reviewed-by: Artem Goncharov <Artem.goncharov@gmail.com>
                 https://github.com/gtema

--- tests/functional/client.test.ts
+++ tests/functional/client.test.ts
@@ -6,6 +6,8 @@ import Client from '../../src/oms'
 import { CloudConfigHelper } from '../../src/oms/core'
 import { IdentityV3 } from '../../src/oms/services/identity/v3'
 import { ImageV2 } from '../../src/oms/services/image'
+import Service from '../../src/oms/services/base'
+import HttpClient from '../../src/oms/core/http'
 
 
 const authUrl = 'https://iam.eu-de.otc.t-systems.com'
@@ -24,16 +26,23 @@ test('Client_auth', async () => {
 test('Client_serviceCatalog', async () => {
     const client = new Client(config!)
     await client.authenticate()
-    expect(Array.from(client.serviceMap.keys())).toHaveLength(Object.keys(client.knownServices).length)
     const iam = client.getService(IdentityV3)
     expect(iam.client.baseConfig.baseURL).toContain('iam.')
     const ims = client.getService(ImageV2)
     expect(ims.client.baseConfig.baseURL).toContain('ims.')
 })
 
+class PseudoIam extends Service {
+    static readonly type = 'thisisnotaserviceyourelookingfor'
+
+    constructor(url: string, client: HttpClient) {
+        super(url, client)
+    }
+}
+
+
 test('Client_invalidService', async () => {
     const client = new Client(config!)
-    client.knownServices.push('identiti')
     await client.authenticate()
-    expect(Array.from(client.serviceMap.keys())).toHaveLength(client.knownServices.length - 1)
+    expect(() => client.getService(PseudoIam)).toThrow(`Service '${PseudoIam.type}' is not registered`)
 })
