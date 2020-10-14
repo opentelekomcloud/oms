d465e3c0939e3b8fad904a77534fdb258bddbe73
Anton Kachurin <anton.kachurin@t-systems.com>
2020-10-14 10:24:58 +0300

    Fix webpack config

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -3,18 +3,6 @@ const path = require('path');
 module.exports = {
     entry: './dist/index.js',
     devtool: 'inline-source-map',
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                use: 'ts-loader',
-                exclude: /node_modules/,
-            },
-        ],
-    },
-    resolve: {
-        extensions: [ '.tsx', '.ts', '.js' ],
-    },
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
