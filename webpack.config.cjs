ff73794b96b2d3faf3deafd74b3b124376127bbd
Anton Kachurin <anton.kachurin@t-systems.com>
2020-10-14 11:24:22 +0300

    Add webpack integration (#40)
    Add webpack integration
    
    Reviewed-by: Anton Kachurin <katchuring@gmail.com>
                 https://github.com/outcatcher

---
+++ webpack.config.cjs
@@ -0,0 +1,11 @@
+const path = require('path');
+
+module.exports = {
+    entry: './dist/index.js',
+    devtool: 'inline-source-map',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+};
+
