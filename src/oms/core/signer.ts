a7180811a9f89333653c06f5ff0c6209fd7e1457
Anton Sidelnikov <a.sidelnikov@t-systems.com>
2020-10-15 14:19:49 +0300

    fix test

--- src/oms/core/signer.ts
+++ src/oms/core/signer.ts
@@ -120,32 +120,3 @@ export class Signature {
         return kSigning;
     }
 }
-
-// function canonicalRequest(config: RequestOpts): string {
-//     const url = new URL(config.url, config.baseURL)
-//     const queryString = url.search.substring(1)
-//     const canonHeaders = Object.entries(config.headers).map(e => `${e[0].toLowerCase()}:${e[1].trim()}\n`).sort().join('')
-//     const signedHeaders = Object.keys(config.headers).sort((a: string, b: string): number => (a[0].toLowerCase() < b[0].toLowerCase() ? -1 : 1)).join(';')
-//     const hash = hexEncode(sha256(config.body!))
-//     return `${config.method}\n${url.pathname}\n${queryString}\n${canonHeaders}\n${signedHeaders}\n${hash}`
-// }
-//
-// function hexEncode(s: string) {
-//     let hex; let i
-//     let result = ''
-//     for (i = 0; i < s.length; i++) {
-//         hex = s.charCodeAt(i).toString(16)
-//         result += (`000${hex}`).slice(-4)
-//     }
-//     return result
-// }
-//
-// export function signRequest(access: string, secret: string) {
-//     return function (config: RequestOpts): RequestOpts {
-//         config.headers.set('X-Amz-Date', new Date()
-//             .toISOString()
-//             .replace(/[:\-]|\.\d{3}/, ''))
-//         config.headers.set('Authorization', canonicalRequest(config))
-//         return config
-//     }
-// }
--- tests/unit/signer.test.ts
+++ tests/unit/signer.test.ts
@@ -1,27 +1,25 @@
-import {Signature, SignatureInputData} from "../../src/oms/core";
+import { Signature, SignatureInputData } from '../../src/oms/core'
 
-test('aws-signature tests', () => {
-    function prepareSignatureInput(): SignatureInputData {
-        const awsSignatureInputData = new SignatureInputData();
-        awsSignatureInputData.method = 'POST';
-        awsSignatureInputData.canonicalUri = '/api/canonical/1';
-        awsSignatureInputData.host = 'test-api.com';
-        awsSignatureInputData.region = 'eu-de';
-        awsSignatureInputData.service = 'crs';
-        awsSignatureInputData.accessKey = 'BmZtmqwC4PPLkWdk20fdhf3hJK1KJqIZtkCGkVPc';
-        awsSignatureInputData.secretKey = 'VFBPWLH41PKM6UPVJTSX';
-        awsSignatureInputData.contentType = 'application/json';
-        awsSignatureInputData.requestParameters = '{"key":"value"}';
-        awsSignatureInputData.canonicalQuerystring = '';
-        return awsSignatureInputData;
+test('aws-signature correct signature', () => {
+    const data: SignatureInputData = {
+        method: 'POST',
+        canonicalUri: '/api/canonical/1',
+        host: 'test-api.com',
+        region: 'eu-de',
+        service: 'crs',
+        accessKey: 'BmZtmqwC4PPLkWdk20fgr4ehJK1KJqIZtkCGkVPc',
+        secretKey: 'GKHYPTS41PKM6UPVJTSX',
+        contentType: 'application/json',
+        requestParameters: '{"key":"value"}',
+        canonicalQuerystring: ''
     }
-
-    const signingTool = new Signature();
-
-    const data = prepareSignatureInput();
-    const date = new Date('2020-10-15:01:00Z');
-    const output = signingTool.generateSignature(data, date);
-    expect(output['Content-Type']).toBe('application/json');
-    expect(output['X-Amz-Date']).toBe('20201015T010000Z');
-    expect(output['Authorization']).toBe('');
+    const signingTool = new Signature()
+    const date = new Date('2020-10-15:01:00Z')
+    const output = signingTool.generateSignature(data, date)
+    expect(output['Content-Type']).toBe('application/json')
+    expect(output['X-Amz-Date']).toBe('20201015T010000Z')
+    expect(output['Authorization']).toBe(
+        'AWS4-HMAC-SHA256 Credential=BmZtmqwC4PPLkWdk20fgr4ehJK1KJqIZtkCGkVPc/' +
+        '20201015/eu-de/crs/aws4_request, SignedHeaders=content-type;host;x-amz-date,' +
+        ' Signature=2cd530a15d8edf2889136672b5ddf141c82be80bc860fbc5a6fd7434abe55c71')
 })
