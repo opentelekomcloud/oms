f115fe7df1fc5ad08e7db04844e3a3c555f78fc0
Anton Kachurin <anton.kachurin@t-systems.com>
2020-10-17 11:46:45 +0300

    Add webpack license plugin (#50)
    Add webpack license plugin
    
    Reviewed-by: Anton Kachurin <katchuring@gmail.com>
                 https://github.com/outcatcher

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -1,4 +1,5 @@
 const path = require('path');
+const LicensePlugin = require('webpack-license-plugin')
 
 module.exports = {
     entry: './dist/index.js',
@@ -8,5 +9,12 @@ module.exports = {
         filename: 'client.min.js',
         path: path.resolve(__dirname, 'dist'),
     },
+    plugins: [
+        new LicensePlugin({
+            excludedPackageTest: (packageName, version) => {
+                return packageName === 'json-schema'
+            },
+        })
+    ],
 };
 
