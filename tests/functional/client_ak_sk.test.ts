4e44e9f08b94049fd46007f45c106373512979ff
Anton Kachurin <anton.kachurin@t-systems.com>
2020-10-23 11:41:04 +0300

    Add project/domain ID resolving for AK/SK auth
    Add functional and integration tests for AK/SK auth

---
+++ tests/functional/client_ak_sk.test.ts
@@ -0,0 +1,22 @@
+import { cloudConfig } from '../../src/oms/core'
+import { Client } from '../../src/oms'
+
+const authUrl = 'https://iam.eu-de.otc.t-systems.com'
+const ak = process.env.AWS_ACCESS_KEY_ID
+if (!ak) {
+    throw 'Missing AWS_ACCESS_KEY_ID required for tests'
+}
+const sk = process.env.AWS_SECRET_ACCESS_KEY
+if (!sk) {
+    throw 'Missing AWS_SECRET_ACCESS_KEY required for tests'
+}
+
+test('Client: ak/sk auth', async () => {
+    const configAkSk = cloudConfig(authUrl)
+        .withAKSK(ak, sk, 'eu-de')
+        .config
+    const clientAkSk = new Client(configAkSk)
+    await clientAkSk.authenticate()
+    expect(clientAkSk.projectID).toBeTruthy()
+    expect(clientAkSk.domainID).toBeTruthy()
+})
